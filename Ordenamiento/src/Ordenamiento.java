8edddfd1c412688f6fb74e3483f46085382b04d1
Ronal Guerrero <ronal.guerrero@gmail.com>
2023-10-24 18:15:39 -0400

    ordenamiento

--- Ordenamiento/src/Ordenamiento.java
+++ Ordenamiento/src/Ordenamiento.java
@@ -1,9 +1,9 @@
 public class Ordenamiento {
-    
-    public void intercambio(int vector[]){        
-        for(int i = 0; i < vector.length; i++){
-            for(int j = i + 1; j < vector.length; j++){
-                if(vector[i] > vector[j]){
+
+    public void intercambio(int vector[]) {
+        for (int i = 0; i < vector.length; i++) {
+            for (int j = i + 1; j < vector.length; j++) {
+                if (vector[i] > vector[j]) {
                     int aux = vector[i];
                     vector[i] = vector[j];
                     vector[j] = aux;
@@ -14,6 +14,22 @@ public class Ordenamiento {
 
     public void seleccion(int vector[]) {
         
+        int i, j, menor, pos, tmp;
+        for (i = 0; i < vector.length - 1; i++) {
+            menor = vector[i];
+            pos = i;
+            for (j = i + 1; j < vector.length; j++) {
+                if (vector[j] < menor) {
+                    menor = vector[j];
+                    pos = j;
+                }
+            }
+            if (pos != i) {
+                tmp = vector[i];
+                vector[i] = vector[pos];
+                vector[pos] = tmp;
+            }
+        }
     }
 
 
